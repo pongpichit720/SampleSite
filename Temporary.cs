d17ae7d219f17d9f013acef5bfae8ecef9c56d91
Pongpichit Poonpiriyasup <pongpichit720@gmail.com>
2020-09-22 00:55:07 -0700

    Add project files.

---
+++ Temporary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleSite
+{
+    public class Temporary
+    {
+        public String Name;
+        
+        public Temporary() {
+            Name = "HELLO WORLD!!!";
+        }
+    }
+}
