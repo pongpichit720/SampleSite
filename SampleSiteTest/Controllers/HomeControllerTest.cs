ba4346b6ce6a1fafbba4d907f4339f0c782d0449
pongp <pongpichit720@gmail.com>
2020-09-30 23:25:54 -0700

    added UT Tests; Controllers and Models

---
+++ SampleSiteTest/Controllers/HomeControllerTest.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SampleSite.Controllers;
+using System.Diagnostics;
+
+namespace SampleSiteTest.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+
+        public static HomeController myController;
+
+        [ClassInitialize]
+        public static void Setup(TestContext context)
+        {
+            myController = new HomeController(null);
+        }
+
+        [TestMethod]
+        public void IndexTest()
+        {
+            Assert.IsNotNull(myController.Index());
+        }
+
+        [TestMethod]
+        public void PrivacyTest()
+        {
+            Assert.IsNotNull(myController.Privacy());
+        }
+
+        [TestMethod]
+        public void Page1Test()
+        {
+            Assert.IsNotNull(myController.Page1());
+        }
+
+        [TestMethod]
+        public void Page2Test()
+        {
+            Assert.IsNotNull(myController.Page2());
+        }
+
+        [TestMethod]
+        public void Page3Test()
+        {
+            Assert.IsNotNull(myController.Page3());
+        }
+
+        [TestMethod]
+        public void Page4Test()
+        {
+            Assert.IsNotNull(myController.Page4());
+        }
+
+        [TestMethod]
+        public void ErrorTest()
+        {
+            Activity activity = new Activity("test");
+            activity.Start();
+
+            var result = myController.Error() as ViewResult;
+
+            activity.Stop();
+
+            Assert.AreEqual("SampleSite.Models.ErrorViewModel", result.Model.ToString());
+        }
+    }
+}
---
+++ SampleSiteTest/Models/ErrorViewModelTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SampleSite.Models;
+
+namespace SampleSiteTest.Models
+{
+    [TestClass]
+    public class ErrorViewModelTest
+    {
+        [TestMethod]
+        public void RequestIdTest()
+        {
+            ErrorViewModel myModel = new ErrorViewModel();
+            Assert.IsNull(myModel.RequestId);
+            myModel.RequestId = "id";
+            Assert.AreEqual("id", myModel.RequestId);
+        }
+
+        [TestMethod]
+        public void ShowRequestIdTest()
+        {
+            ErrorViewModel myModel = new ErrorViewModel();
+            Assert.IsFalse(myModel.ShowRequestId);
+            myModel.RequestId = "id";
+            Assert.IsTrue(myModel.ShowRequestId);
+        }
+
+    }
+}
